4c4a6c8870b2550e23a543c032d7490835bab6a4
DeViS1303 <lushantima@gmail.com>
2020-03-20 14:13:49 +0200

    Init

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+let gulp = require('gulp'),
+  minCSS = require('gulp-clean-css'),
+  browserSync = require('browser-sync').create(),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  autopref = require('gulp-autoprefixer'),
+  minHTML = require('gulp-htmlclean');
+
+//Puth
+let fromScss = 'dev/scss/**/*.scss',
+    styleScss = 'dev/scss/style.scss',
+    toCSS = 'prod/styles/',
+    toMinCSS = 'prod/styles/',
+    fromHTML = 'dev/*.html',
+    toMinHTML = 'prod/';
+
+
+gulp.task('html', function() {
+  return gulp.src(fromHTML)
+    .pipe(minHTML())
+    .pipe(gulp.dest('prod/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', function() {
+  return gulp.src(styleScss)
+    .pipe(sass())
+    .pipe(autopref({
+      browsers: ['last 2 versions'],
+      cascade: false
+    })) 
+    .pipe(gulp.dest(toCSS))
+    .pipe(minCSS())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest(toMinCSS))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function() {
+
+  browserSync.init({
+    server: "prod/"
+  });
+      
+  gulp.watch(fromScss, gulp.parallel('sass'));
+  gulp.watch(fromHTML, gulp.parallel('html'));
+});
+  
+gulp.task('default', gulp.series('html','sass','serve'));
